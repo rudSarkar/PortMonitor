b2a50d2613afc63e83605705a88a6286c6e1716d
Rudra Sarkar <rudrasarkar815@gmail.com>
2022-08-22 04:30:48 +0600

    fixed environment location to /Users/rudra/.portmonitor/.env

--- checkenv.go
+++ checkenv.go
@@ -3,10 +3,11 @@ package main
 import (
 	"github.com/joho/godotenv"
 	"github.com/thep0y/go-logger/log"
+	"os"
 )
 
 func checkDotEnv() bool {
-	if err := godotenv.Load(".env"); err != nil {
+	if err := godotenv.Load(os.ExpandEnv("$HOME/.portmonitor/.env")); err != nil {
 		log.Fatal("No .env file found")
 	}
 
